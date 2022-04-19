dadcfa904d1a09be2b9e31fa9c6c8e48fb4e4b8d
Rohan Girish <rohan@pop-os.localdomain>
2022-04-19 16:46:40 +0530

    fixed assignment

--- 2021101042_a1/Q2.sh
+++ 2021101042_a1/Q2.sh
@@ -7,5 +7,5 @@ IFS="~"
 while read -r line;
 do
     read -a q <<< "$line"
-    echo "${q[1]} once said, ${q[0]}">>"speech.txt"
+    echo "${q[1]} once said, \"${q[0]}\""| sed 's/ \(.\)$/\1/' >>"speech.txt"
 done <$file
